d629f8745b0f84d2ddbfdb6da0e9187bc6d46971
tsago123 <zaal.tsagareli.1@btu.edu.ge>
2022-09-13 15:32:52 +0400

    added radio handling

--- index.js
+++ index.js
@@ -28,12 +28,41 @@ const validationFunctions = {
   },
 };
 
+function check(id) {
+  document.getElementById(id).checked = true;
+  return true;
+}
+
+function uncheck(id) {
+  document.getElementById(id).checked = false;
+  return true;
+}
+
 const button = document.getElementById("submit");
 const nameInput = document.getElementById("name");
 const lastNameInut = document.getElementById("lastName");
 const addressInput = document.getElementById("address");
+const maleRadio = document.getElementById("maleRadio");
+const femaleRadio = document.getElementById("femaleRadio");
 const items = document.getElementById("items");
 
+let choosenGender = "";
+
+const changeHandler = (gender) => {
+  if (gender === "male") {
+    maleRadio.checked && uncheck("femaleRadio")
+      ? (choosenGender = "male")
+      : (choosenGender = "");
+  }
+
+  if (gender === "female") {
+    femaleRadio.checked && uncheck("maleRadio")
+      ? (choosenGender = "female")
+      : (choosenGender = "");
+  }
+
+  console.log(choosenGender);
+};
 function allStorage() {
   var values = [],
     keys = Object.keys(localStorage),
@@ -56,7 +85,11 @@ const loadHandler = () => {
       console.log(obj);
 
       const oneItem = document.createElement("div");
-      oneItem.innerHTML = `name : ${obj.name}; lastname : ${obj.lastName}; address : ${obj.address}`;
+      oneItem.innerHTML = `name : ${obj.name}; lastname : ${
+        obj.lastName
+      }; address : ${obj.address}; gender : ${
+        obj.choosenGender ? obj.choosenGender : "not defined"
+      }`;
 
       items.appendChild(oneItem);
     });
@@ -64,9 +97,10 @@ const loadHandler = () => {
 };
 
 const clickHandler = () => {
+  //name validation
   let name = nameInput.value;
   let nameIsValid = validationFunctions.nameValidation(name);
-  //lastname
+  //lastname validation
   let lastName = lastNameInut.value;
   let lastNameIsValid = validationFunctions.lastNameValidation(lastName);
   //address Validation
@@ -75,11 +109,12 @@ const clickHandler = () => {
 
   if (nameIsValid && lastNameIsValid && isAddressValid) {
     const items = allStorage();
-
-    localStorage.setItem(
-      items.length + 1,
-      JSON.stringify({ name: name, lastName: lastName, address: address })
-    );
+    let item = { name: name, lastName: lastName, address: address };
+    console.log(choosenGender);
+    if (choosenGender != "") {
+      item.choosenGender = choosenGender;
+    }
+    localStorage.setItem(items.length + 1, JSON.stringify(item));
     loadHandler();
   }
 };
